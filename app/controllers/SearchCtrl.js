8a40f0ebdffb6f5aedbc6606ab1402db67cbe72d
Kirk Walker Graves <graves.kirk@gmail.com>
2016-02-04 20:57:10 -0600

    used ng-repeat to display titles from api in DOM

--- app/controllers/SearchCtrl.js
+++ app/controllers/SearchCtrl.js
@@ -6,24 +6,21 @@ app.controller('SearchCtrl', ['$http', '$scope', function($http, $scope) {
 		$http.get('https://www.googleapis.com/books/v1/volumes?q=' + $scope.titleQuery)
 		.then(function(response) {
 			console.log('response', response);
-			$scope.titleList = response.data.items[0].volumeInfo.title;
+			$scope.titleList = response.data.items;
 			console.log('$scope.titleList', $scope.titleList);
 		});
 
-	
-
-
-
 	};
 
+	$scope.searchByAuthor = function() {
 
+		$http.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:' + $scope.authorQuery + '&key=AIzaSyCD11RSLksVZrfVRQLkjvMFMW7ucyzVi8Y')
+		.then(function(response) {
+			$scope.authorsList = response.data.items;
+		});	
+	};
 
-
-
-
-
-
-
+	
 
 
 
